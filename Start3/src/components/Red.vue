2986158935b4804097c5a3ac1f349cc69dcb3e99
Elise Griset <elise.griset@gmail.com>
2018-06-01 16:11:56 +0200

    exercises on vue

---
+++ Start3/src/components/Red.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       <p> hello red ! </p>
+ 
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+  background-color: lightcoral;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
